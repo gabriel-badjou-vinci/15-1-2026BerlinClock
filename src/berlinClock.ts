759f97f79368528ef62e1b32c58f1c60d08e4eb7
Gabriel Badjou <gabriel.student.vinci.be>
2026-01-15 14:22:18 +0100

    refactor: manage multiple of 3

--- src/berlinClock.ts
+++ src/berlinClock.ts
@@ -11,20 +11,14 @@ export class BerlinClockClass {
         };
         this.isMultipleOfFive(time, clock);
         this.isMultipleOfOne(time, clock);
-        
         this.isMultipleOfTwo(time, clock);
-        if(time.charAt(4) === '3'){
+        if (Number(time.charAt(4)) % 5 === 3) {
             clock.minutes = "YYY_";
         }
         if(time.charAt(4) === '4'){
             clock.minutes = "YYYY";
         }
-        if(time.charAt(4) === '7'){
-            clock.minutes = "YY__";
-        }
-        if(time.charAt(4) === '8'){
-            clock.minutes = "YYY_";
-        }
+       
         if(time.charAt(4) === '9'){
             clock.minutes = "YYYY";
         }
